f60bad2b482c0b53d92a91cb44349def0676c549
Chrsler <100032593+Chrsler@users.noreply.github.com>
2023-05-07 19:40:13 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import 'flowbite';
+
+module.exports = {
+
+    plugins: [
+        require('flowbite/plugin')
+    ]
+    
+    content: [
+        "./node_modules/flowbite/**/*.js"
+    ]
+
+}
+
+
+
